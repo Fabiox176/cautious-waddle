21db7203054646ac9376a64da707de27cb30aa1d
Fabio Romero <fabiofr176@outlook.es>
2022-07-15 23:03:05 -0300

    se agrego constructor, gett and sett

--- src/main/java/com/mycompany/mavenproject1/objects/DNI.java
+++ src/main/java/com/mycompany/mavenproject1/objects/DNI.java
@@ -12,5 +12,38 @@ public class DNI {
     private String nombre;
     private String apellido;
     private int numero;
+
+    public DNI(String nombre, String apellido, int numero) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.numero = numero;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+    
+    
+    
     
 }
