c22e94db8d3ab7e6967c4e8e022a824f32e22344
Fabio Romero <fabiofr176@outlook.es>
2022-07-15 22:45:39 -0300

    version1

---
+++ src/main/java/com/mycompany/mavenproject1/objects/DNI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.mavenproject1.objects;
+
+/**
+ *
+ * @author Fabiox
+ */
+public class DNI {
+    private String nombre;
+    private String apellido;
+    private int numero;
+    
+}
